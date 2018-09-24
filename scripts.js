f2dd20cccf57e4875c94e2b2e477501e3f77f1e9
Slawek <Stefcio7@gmail.com>
2018-09-24 22:44:59 +0100

    add base file structure

---
+++ scripts.js
@@ -0,0 +1,31 @@
+function colorClock(){
+var date = new Date();
+
+var hours = date.getHours();
+var minutes = date.getMinutes();
+var seconds = date.getSeconds();
+
+if (hours < 10) {
+  hours = '0' + hours;
+}
+if (minutes < 10) {
+  minutes = '0' + minutes;
+}
+if (seconds < 10) {
+  seconds = '0' + seconds;
+}
+var hexColor = '#' + hours + minutes + seconds;   
+var clockFace = hours + ':' + minutes + ':' + seconds;
+
+document.body.style.background = hexColor;
+document.getElementById('clock').innerHTML = clockFace;
+  
+  setTimeout(function() {
+  colorClock();
+}, 1000);
+  
+}
+
+colorClock();
+
+
